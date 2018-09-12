c02e1cd541083ab02fdc0eae6d65f9f03d8d7ed2
Adam T. Croft <sendtoatc@gmail.com>
2018-09-12 11:58:52 -0700

    fixing merge

--- waapinode.ts
+++ waapinode.ts
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 export {Wwise} from './source/wwise';
-=======
-export {Wwise} from './source/wwise';
->>>>>>> 0f84cbf737a4cb9ebda7b37c53503db6ba9a5c95
+export{WwisePacket} from './source/wwisepacket';
