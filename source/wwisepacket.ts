11389d85a9baca562670d5f21be24457571823f9
Adam T. Croft <sendtoatc@gmail.com>
2018-09-13 21:53:30 -0700

    Adding functions

--- source/wwisepacket.ts
+++ source/wwisepacket.ts
@@ -10,4 +10,15 @@ export class WwisePacket {
 
 export enum Functions {
     getInfo = "ak.wwise.core.getInfo",
+    copy = "ak.wwise.core.object.copy",
+    get = "ak.wwise.core.object.get",
+    create = "ak.wwise.core.object.create",
+    delete = "ak.wwise.core.object.delete",
+    executeActionOnEvent = "ak.soundengine.executeActionOnEvent",
+    postEvent = "ak.soundengine.postEvent",
+    postMsgMonitor = "ak.soundengine.postMsgMonitor",
+    postTrigger = "ak.soundengine.postTrigger",
+    registerGameObj = "ak.soundengine.registerGameObj",
+    resetRTPCValue = "ak.soundegine.resetRTPCValue",
+    seekOnEvent = "ak.soundengine.seekOnEvent"
 }
