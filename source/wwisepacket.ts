48fe6806976fd8306aa28a95f6828acf0913ef5c
Adam T. Croft <sendtoatc@gmail.com>
2018-09-16 18:11:04 -0700

    bunch of updates to the functions

--- source/wwisepacket.ts
+++ source/wwisepacket.ts
@@ -1,34 +1,11 @@
+import {WwiseFunctions} from './wwisefunctions';
+
 export class WwisePacket {
-    functionCall: Functions;
+    functionCall: WwiseFunctions;
     arguments;
     options;
 
     constructor(functionCall) {
         this.functionCall = functionCall;
     }
-}
-
-export enum Functions {
-    getInfo = "ak.wwise.core.getInfo",
-    copy = "ak.wwise.core.object.copy",
-    get = "ak.wwise.core.object.get",
-    create = "ak.wwise.core.object.create",
-    delete = "ak.wwise.core.object.delete",
-    executeActionOnEvent = "ak.soundengine.executeActionOnEvent",
-    postEvent = "ak.soundengine.postEvent",
-    postMsgMonitor = "ak.soundengine.postMsgMonitor",
-    postTrigger = "ak.soundengine.postTrigger",
-    registerGameObj = "ak.soundengine.registerGameObj",
-    resetRTPCValue = "ak.soundegine.resetRTPCValue",
-    seekOnEvent = "ak.soundengine.seekOnEvent",
-    setDefaultListeners = "ak.soundengine.setDefaultListeners",
-    setGameObjectAuxSendValues = "ak.soundengine.setGameObjectAuxSendValues",
-    setGameObjectOutputBusVolume = "ak.soundengine.setGameObjectOutputBusVolume",
-    setListeners = "ak.soundengine.setListeners",
-    setListenerSpatialization = "ak.soundengine.setListenerSpatialization",
-    setMultiplePositions = "ak.soundengine.setMultiplePositions",
-    setObjectObstructionAndOcclusion = "ak.soundengine.setObjectObstructionAndOcclusion",
-    setPosition = "ak.soundengine.setPosition",
-    setRTPCValue = "ak.soundengine.setRTPCValue",
-    setScalingFactor = "ak.soundengine.setScalingFactor",
 }
