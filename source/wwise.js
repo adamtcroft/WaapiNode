07152f07d66ca8ab46936e82855b252a2e9c414f
Adam T. Croft <sendtoatc@gmail.com>
2018-09-27 16:17:04 -0700

    refactor

--- source/wwise.js
+++ source/wwise.js
@@ -1,4 +1,4 @@
-//const autobahn = require('autobahn');
+const autobahn = require('autobahn');
 //const waapi = require('waapi');
 //const waapiClient = require('waapi-client');
 //const { WwiseFunctions } = require("./wwisefunctions");
@@ -6,9 +6,33 @@
 
 
 class Wwise {
-    constructor()
-    {
-        console.log("We Made a Wwise!");
+    constructor() {
+        this.connection = new autobahn.Connection({
+            url: 'ws://localhost:8080/waapi',
+            realm: 'realm1',
+            protocols: ['wamp.2.json']
+        });
+    }
+
+    send() {
+        connection.onopen = (session) => {
+            session.call('ak.wwise.core.getInfo', [], {}).then(
+                function (res) {
+                    console.log(res);
+                },
+                function (error) {
+                    console.log(error);
+                }
+            ).then(
+                {
+                    function() {
+                        connection.close();
+                    }
+                }
+            );
+        };
+        
+        connection.open();
     }
 }
 
